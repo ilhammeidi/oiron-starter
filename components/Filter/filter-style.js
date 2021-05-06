0c219f2c3a977cb854d208e7c8eed47e6d78aae9
ilhammeidi <ilham.meidi@gmail.com>
2021-05-07 06:32:17 +0700

    finish filter

--- components/Filter/filter-style.js
+++ components/Filter/filter-style.js
@@ -19,6 +19,19 @@ const filterStyles = makeStyles(theme => ({
       width: '100%',
       marginRight: 0
     }
+  },
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
   }
 }));
 
